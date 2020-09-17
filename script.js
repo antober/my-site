5edf4768b2c4cffd11a08b07f3b76d2b744781a5
Anton Öberg <ao222qg@student.lnu.se>
2020-09-18 00:52:11 +0200

    Fix.

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-const academicWork = [new Date(2019, 9, 3), new Date(2020, 11, 4)]
-const jetshop = [new Date(2020, 0, 7), "present"]
+const academicWork = [new Date(2019, 9, 3), new Date(2020, 11, 4)];
+const jetshop = [new Date(2020, 0, 7), "present"];
 var cursor = true;
 var speed = 500;
 
 
 function CreateRowNumbers()
 {
-    let rows = 18
+    let rows = 18;
     for(let i = 0; i < rows; i++) {
         let number = document.createElement("li");
         number.id = 'number';
         document.getElementById("list").appendChild(number);
     }
-}
+};
 
 function GetWorkDates(dates) {
     if (dates[1] === "present")
@@ -20,7 +20,7 @@ function GetWorkDates(dates) {
         return dates[0].toDateString() + " - " + dates[1];
     }
     return dates[0].toDateString() + " - " + dates[1].toDateString();
-}
+};
 
 setInterval(() => {
     if (cursor) {
@@ -32,7 +32,7 @@ setInterval(() => {
     }
 }, speed);
 
-CreateRowNumbers()
+CreateRowNumbers();
 
 document.getElementById("workDatesAcademicWork").innerHTML = GetWorkDates(academicWork);
 document.getElementById("workDatesJetshop").innerHTML = GetWorkDates(jetshop);
